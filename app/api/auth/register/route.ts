cbbc36a2c0a49baf9542914f6403ecfe767034c7
sonualam <sonualam1599@gmail.com>
2025-02-02 20:56:01 +0530

    add user register auth

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: "Email and Password are required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "Email is already registered",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await User.create({ email, password });
+
+    return NextResponse.json(
+      {
+        message: "User registered successfully",
+      },
+      {
+        status: 201,
+      }
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Failed to register User",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
